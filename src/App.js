c9700c034be3dd4e74f41a6ea7ed17a2bad8879f
unknown <nicolas.cuatrini@gmail.com>
2022-08-12 10:29:24 -0300

    challenge ready menos toggle text color

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import { useState } from "react";
 function App() {
   const [colorValue, setColorValue] = useState('')
   const [hexValue, setHexValue] = useState('')
+  const [isDarkText, setIsDarkText] = useState(true)
   return (
     <div className="App">
       <Square 
         colorValue={colorValue}
-        hexValue={hexValue}/>
+        hexValue={hexValue}
+        isDarkText={isDarkText}/>
       <Input
         colorValue={colorValue}
         setColorValue={setColorValue}
-        sethexValue={setHexValue}
+        setHexValue={setHexValue}
+        isDarkText={isDarkText}
+        setDar
       />
     </div>
   );
--- src/Input.js
+++ src/Input.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import colorNames from 'colornames'
-const Input = ({colorValue, setColorValue, setHexValue}) => {
+const Input = ({colorValue, setColorValue, setHexValue, isDarkText, setIsDarkText}) => {
     
   return (
     <form onSubmit={(e) => e.preventDefault()}>
@@ -16,6 +16,12 @@ const Input = ({colorValue, setColorValue, setHexValue}) => {
               setHexValue(colorNames(e.target.value));
             }}
         />
+        <button
+            type="button"
+            onClick={() => setIsDarkText(!isDarkText)}
+          > 
+           Toggle Text Color
+        </button>
     </form>
   )
 }
